5f3bc767a43ba9264ac7145d08bfab6ff67642c1
vwiart <vincent.wiart@gmail.com>
2016-07-29 12:15:05 +0200

    add comments

--- docker-install.sh
+++ docker-install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#install docker engine on a debian jessie
+#cf. https://docs.docker.com/engine/installation/linux/debian/
 sudo apt-get update && \
 	sudo apt-get install -y --no-install-recommends apt-transport-https ca-certificates && \
 	sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
