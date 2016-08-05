64556c685d8df1e2c84ed9bf4dee6f8fdf3188bb
vwiart <vincent.wiart@gmail.com>
2016-08-05 12:24:43 +0200

    add labels

--- setup-sys.sh
+++ setup-sys.sh
@@ -13,9 +13,11 @@ function setupUser() {
 	useradd -ms /bin/zsh vwiart -g users -g ssh -g sudo
 	git clone https://github.com/vwiart/bash-script.git /home/vwiart/bash-script
 	chown vwiart:users -R /home/vwiart
+	echo Choose vwiart password
 	passwd vwiart
 }
 
+echo Choose root password
 passwd
 initsys
 setupUser
