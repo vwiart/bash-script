16ee6dd95e2f3e862df634dfbd73142f8e71f68f
vwiart <vincent.wiart@gmail.com>
2016-08-05 12:05:18 +0200

    zsh: add honukai theme

--- setup-user.sh
+++ setup-user.sh
@@ -3,6 +3,10 @@
 function setupZSH() {
 	wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 	chsh -s `which zsh`
+	cp -R resources/.oh-my-zsh ~vwiart/
+	cp -R resources/.zshrc ~vwiart/
+	cp resources/honukai.zsh-theme ~vwiart/.oh-my-zsh/themes/honukai.zsh-theme
+	source ~vwiart/.zshrc
 }
 
 setupZSH
