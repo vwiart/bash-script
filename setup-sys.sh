d57c9dfc4f7fae7946d65c0ac8202fa36d66c841
vwiart <vincent.wiart@gmail.com>
2016-09-23 11:28:28 +0200

    add deps : gcc, build-essential

--- setup-sys.sh
+++ setup-sys.sh
@@ -6,7 +6,7 @@ function initsys() {
 	apt-get dist-upgrade -y
 	apt-get clean
 	apt-get autoremove
-	apt-get install -y --no-install-recommends curl git apt-transport-https ca-certificates zsh sudo tmux
+	apt-get install -y --no-install-recommends curl git apt-transport-https ca-certificates zsh sudo tmux gcc build-essential
 }
 
 function setupUser() {
