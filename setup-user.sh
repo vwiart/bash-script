88de0b67b78139c546a6542a3570e8864827fc8c
Vincent Wiart <vincent.wiart@gmail.com>
2016-09-30 18:07:14 +0200

    alias git grog

--- setup-user.sh
+++ setup-user.sh
@@ -4,6 +4,7 @@ HOME=~vwiart
 function setupGit() {
 	git config --global user.email "vincent.wiart@gmail.com"
 	git config --global user.name "vwiart"
+	git config --global alias.grog 'log --graph --abbrev-commit --decorate --all --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)"'
 }
 
 function setupZSH() {
